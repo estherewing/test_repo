d2c5d805239b4f43a31fd3f1c8d955d723d93145
Esther Kim <ek8435@educcomp04.ccbb.utexas.edu>
2020-12-04 10:30:38 -0600

    made some changes

--- testfile.py
+++ testfile.py
@@ -2,3 +2,10 @@ print('this is my test file before I build my website')
 
 ## Adding a comment
 
+#How to update changes to github
+  #save the file
+  #run:
+    #git add testfile.py
+    #git commit -m "made some changes"
+    #git push origin master
+  #log in
